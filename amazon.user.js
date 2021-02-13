36676a6eadbabbc119762a3941e2add9828cea4e
Dan Nelson <dnelson_1901@yahoo.com>
2021-02-13 16:30:27 -0600

    add another ad class

--- amazon.user.js
+++ amazon.user.js
@@ -3,7 +3,7 @@
 // @description make it more obvious which entries are ads
 // @downloadURL https://github.com/dnelson-1901/greasemonkey-scripts/raw/master/amazon.user.js
 // @include     https://www.amazon.com/*
-// @version     1.2020.09.13.1
+// @version     1.2021.02.13.1
 // @grant       none
 // ==/UserScript==
 
@@ -20,6 +20,7 @@ var css = `
 [data-component-type^="sbv-"],
 [class^="sbv-"],
 [data-ad-id]
+[data-aid]
 {
 	outline: solid 3px #ddbb88;
 	background-image: repeating-linear-gradient(45deg, #fff, #fff 0.5em,#ffeedd 0.5em,#ffeedd 0.6em );

b8d48377cda1a5dcbd1867ca42c3306da3aecbca
Dan Nelson <dnelson_1901@yahoo.com>
2022-04-14 19:58:03 -0500

    make the button a button tag, clean up code a bit

--- discord.user.js
+++ discord.user.js
@@ -3,28 +3,37 @@
 // @description Allow collapsing of sidebar
 // @downloadURL https://github.com/dnelson-1901/greasemonkey-scripts/raw/master/discord.user.js
 // @include     https://*.discord.com/channels/*
-// @version     1.2022.04.14.2
+// @version     1.2022.04.14.3
 // @grant       none
 // ==/UserScript==
 
 function addbutton()
 {
-  document
-    .querySelector("[class^=sidebar-]")
-    .insertAdjacentHTML(
-      "afterend",
-      '<div><div id=toggle style="position: absolute;z-index: 100;">[<]</div></div>'
-    );
+  /* A button that floats above all other content. */
+  var button=`
+<div>
+  <button id=toggle style="position: absolute; z-index: 100;">
+    <<
+  </button>
+</div>
+`;
+
+  /* Find the sidebar.  All discord classes have random garbage at the end,
+   * so we have to use a prefix match.  */
+  var s = document.querySelector("[class^=sidebar-]");
+
+  /* Insert our button right after it. */
+  s.insertAdjacentHTML("afterend", button);
   console.log("added button");
   
+  /* Clicking on the button toggles the sidebar on and off */
   document.getElementById("toggle").onclick = function (e) {
-    var s = document.querySelector("[class^=sidebar-]");
     if (s.style.display == "none") {
       s.style.display = "";
-      e.target.innerText = "[<]";
+      e.target.innerText = "<<";
     } else {
       s.style.display = "none";
-      e.target.innerText = "[>]";
+      e.target.innerText = ">>";
     }
   };
   console.log("added handler");
